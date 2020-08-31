73465d04ba2f56cc94d98af0ad9d00aaafba0959
semalai <m.semalaiappan@gmail.com>
2020-08-31 11:15:44 -0500

    review comment fix

--- EdFi.Ods.Utilities.Migration/Scripts/02 Upgrade/v34_to_v50/01 Structure/1010-AddNewDescriptorValuesForV50.sql
+++ EdFi.Ods.Utilities.Migration/Scripts/02 Upgrade/v34_to_v50/01 Structure/1010-AddNewDescriptorValuesForV50.sql
@@ -25,6 +25,9 @@ VALUES ('uri://ed-fi.org/IndicatorLevelDescriptor', 'Medium Retention', 'Medium 
 INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
 VALUES ('uri://ed-fi.org/IndicatorLevelDescriptor', 'High Retention', 'High Retention', 'Staff retention from previous school year is between 90-100 percent of staff retained');
 
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/IndicatorLevelDescriptor', 'Low Retention', 'Low Retention', 'Staff retention from previous school year is under 80 percent of staff retained');
+
 
 INSERT INTO edfi.IndicatorLevelDescriptor
 SELECT DescriptorId FROM edfi.Descriptor
@@ -37,6 +40,24 @@ VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Checkbox', 'Checkbox', 'Check
 INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
 VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Dropdown', 'Dropdown', 'Dropdown');
 
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Matrix of numeric ratings', 'Matrix of numeric ratings', 'Matrix of numeric ratings');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Radio box', 'Radio box', 'Radio box');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Matrix of dropdowns', 'Matrix of dropdowns', 'Matrix of dropdowns');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Ranking', 'Ranking', 'Ranking');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Textbox', 'Textbox', 'Textbox');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/QuestionFormDescriptor', 'Matrix of textboxes', 'Matrix of textboxes', 'Matrix of textboxes');
+
 INSERT INTO edfi.QuestionFormDescriptor
 SELECT DescriptorId FROM edfi.Descriptor
 WHERE [Namespace] = 'uri://ed-fi.org/QuestionFormDescriptor';
@@ -47,6 +68,12 @@ VALUES ('uri://ed-fi.org/SourceSystemDescriptor', 'District', 'District', 'Distr
 INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
 VALUES ('uri://ed-fi.org/SourceSystemDescriptor', 'School', 'School', 'School');
 
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SourceSystemDescriptor', 'State', 'State', 'State');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SourceSystemDescriptor', 'Federal', 'Federal', 'Federal');
+
 INSERT INTO edfi.SourceSystemDescriptor
 SELECT DescriptorId FROM edfi.Descriptor
 WHERE [Namespace] = 'uri://ed-fi.org/SourceSystemDescriptor';
@@ -55,7 +82,31 @@ INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Descript
 VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Applicant', 'Applicant', 'Applicant');
 
 INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Administrator', 'Administrator', 'Administrator');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'District', 'District', 'District');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
 VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Community', 'Community', 'Community');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Other', 'Other', 'Other');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Exit', 'Exit', 'Exit');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Parent', 'Parent', 'Parent');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Principal', 'Principal', 'Principal');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Student', 'Student', 'Student');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyCategoryDescriptor', 'Teacher', 'Teacher', 'Teacher');
 
 INSERT INTO edfi.SurveyCategoryDescriptor
 SELECT DescriptorId FROM edfi.Descriptor
@@ -67,6 +118,74 @@ VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Adult Education', 'Adult Educa
 INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
 VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Eighth grade', 'Eighth grade', 'Eighth grade');
 
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Eleventh grade', 'Eleventh grade', 'Eleventh grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Adult Education', 'Adult Education', 'Adult Education');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'First grade', 'First grade', 'First grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Fourth grade', 'Fourth grade', 'Fourth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Infant/toddler', 'Infant/toddler', 'Infant/toddler');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Fifth grade', 'Fifth grade', 'Fifth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Grade 13', 'Grade 13', 'Grade 13');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Kindergarten', 'Kindergarten', 'Kindergarten');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Postsecondary', 'Postsecondary', 'Postsecondary');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Other', 'Other', 'Other');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Ninth grade', 'Ninth grade', 'Ninth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Second grade', 'Second grade', 'Second grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Seventh grade', 'Seventh grade', 'Seventh grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Preschool/Prekindergarten', 'Preschool/Prekindergarten', 'Preschool/Prekindergarten');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Sixth grade', 'Sixth grade', 'Sixth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Twelfth grade', 'Twelfth grade', 'Twelfth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Third grade', 'Third grade', 'Third grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Tenth grade', 'Tenth grade', 'Tenth grade');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Master''s', 'Master''s', 'Master''s');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'No grade level', 'No grade level', 'No grade level');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Ungraded', 'Ungraded', 'Ungraded');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Professional Certification', 'Professional Certification', 'Professional Certification');
+
+INSERT INTO edfi.Descriptor ([Namespace], CodeValue, ShortDescription, [Description])
+VALUES ('uri://ed-fi.org/SurveyLevelDescriptor', 'Undergraduate', 'Undergraduate', 'Undergraduate');
 INSERT INTO edfi.SurveyLevelDescriptor
 SELECT DescriptorId FROM edfi.Descriptor
 WHERE [Namespace] = 'uri://ed-fi.org/SurveyLevelDescriptor';
