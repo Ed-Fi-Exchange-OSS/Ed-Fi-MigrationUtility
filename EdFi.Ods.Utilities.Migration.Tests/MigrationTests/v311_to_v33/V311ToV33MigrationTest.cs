07f3988bae003307ae9437c323b1288ddfe30dd1
travis.meador <t.meador@wearedoubleline.com>
2020-10-07 13:41:54 -0500

    revert test version changes

--- EdFi.Ods.Utilities.Migration.Tests/MigrationTests/v311_to_v33/V311ToV33MigrationTest.cs
+++ EdFi.Ods.Utilities.Migration.Tests/MigrationTests/v311_to_v33/V311ToV33MigrationTest.cs
@@ -10,14 +10,13 @@ using EdFi.Ods.Utilities.Migration.Configuration;
 using EdFi.Ods.Utilities.Migration.Enumerations;
 using EdFi.Ods.Utilities.Migration.MigrationManager;
 using EdFi.Ods.Utilities.Migration.Tests.MigrationTests.Models.v33;
-
-namespace EdFi.Ods.Utilities.Migration.Tests.MigrationTests.v32_to_v33
+namespace EdFi.Ods.Utilities.Migration.Tests.MigrationTests.v311_to_v33
 {
-    public abstract class V32ToV33MigrationTest : MigrationTestBase
+    public abstract class V311ToV33MigrationTest : MigrationTestBase
     {
-        protected override EdFiOdsVersion FromVersion => EdFiOdsVersion.V32;
+        protected override EdFiOdsVersion FromVersion => EdFiOdsVersion.V311;
         protected override EdFiOdsVersion ToVersion => EdFiOdsVersion.V33;
-        protected override string TestDataDirectoryName => "v32_to_v33";
+        protected override string TestDataDirectoryName => "v311_to_v33";
 
         protected OdsUpgradeResult PerformTestMigration(string sourceDataScriptName = null, DynamicParameters scriptParameters = null, string calendarConfigurationFileName = null, string namespacePrefix = null)
         {
@@ -30,7 +29,7 @@ namespace EdFi.Ods.Utilities.Migration.Tests.MigrationTests.v32_to_v33
                 UpgradeVersionConfiguration.BuildValidUpgradeConfiguration(ConnectionString,
                     FromVersion.ToString(), ToVersion.ToString());
 
-            var config = new MigrationConfigurationV32ToV33
+            var v311ToV32Config = new MigrationConfigurationV311ToV32
             {
                 DatabaseConnectionString = ConnectionString,
                 BaseMigrationScriptFolderPath = Path.GetFullPath(MigrationTestSettingsProvider.GetConfigVariable("BaseMigrationScriptFolderPath")),
@@ -39,7 +38,20 @@ namespace EdFi.Ods.Utilities.Migration.Tests.MigrationTests.v32_to_v33
                 Timeout = SqlCommandTimeout
             };
 
-            var migrationManager = new OdsMigrationManagerV32ToV33(config, versionConfiguration);
+            var v32ToV33Config = new MigrationConfigurationV32ToV33
+            {
+                DatabaseConnectionString = ConnectionString,
+                BaseMigrationScriptFolderPath = Path.GetFullPath(MigrationTestSettingsProvider.GetConfigVariable("BaseMigrationScriptFolderPath")),
+                BaseDescriptorXmlDirectoryPath = Path.GetFullPath(MigrationTestSettingsProvider.GetConfigVariable("BaseDescriptorXmlDirectoryPath")),
+                BypassExtensionValidationCheck = false,
+                Timeout = SqlCommandTimeout
+            };
+
+            var migrationManager = new List<IOdsMigrationManager>
+            {
+                new OdsMigrationManagerV311ToV32(v311ToV32Config, versionConfiguration),
+                new OdsMigrationManagerV32ToV33(v32ToV33Config, versionConfiguration),
+            };
             return RunMigration(migrationManager);
         }
 
