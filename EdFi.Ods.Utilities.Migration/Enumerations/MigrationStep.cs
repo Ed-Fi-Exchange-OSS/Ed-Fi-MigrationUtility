b792b42d46ea541c4fd919ded069019d48057918
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2020-11-16 14:16:33 -0600

    [ODS-4500] Adjust naming convention for scripts (#18)
    * Adjusted Namins Convention for scripts
    
    * MigrationStep code fix

--- EdFi.Ods.Utilities.Migration/Enumerations/MigrationStep.cs
+++ EdFi.Ods.Utilities.Migration/Enumerations/MigrationStep.cs
@@ -8,12 +8,12 @@ namespace EdFi.Ods.Utilities.Migration.Enumerations
     public sealed class MigrationStep : Enumeration<MigrationStep>
     {
         public static MigrationStep Setup = new MigrationStep(0, "Setup", VersionTarget.VersionSpecific);
-        public static MigrationStep CompatibilityCheck = new MigrationStep(1, "Compatibility Check", VersionTarget.VersionSpecific);
+        public static MigrationStep CompatibilityCheck = new MigrationStep(1, "Precheck", VersionTarget.VersionSpecific);
         public static MigrationStep Upgrade = new MigrationStep(2, "Upgrade", VersionTarget.VersionSpecific);
         public static MigrationStep Cleanup = new MigrationStep(3, "Cleanup", VersionTarget.AllVersions);
 
         public int ExecutionOrder => Value;
-        public string FolderName => $"{ExecutionOrder:D2} {DisplayName}";
+        public string FolderName => $"{ExecutionOrder:D2}{DisplayName}";
         public VersionTarget ScriptVersionTarget { get; }
 
         public enum VersionTarget
