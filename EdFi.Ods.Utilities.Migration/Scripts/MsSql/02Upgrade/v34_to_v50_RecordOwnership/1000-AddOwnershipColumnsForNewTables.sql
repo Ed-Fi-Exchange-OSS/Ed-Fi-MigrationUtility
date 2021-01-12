bb44d5387614b6e2ac6e328c6508b546392ba3b0
Benjamin Meyers <blmeyers@users.noreply.github.com>
2021-01-12 11:24:33 -0500

    Adding Sql Server migration support for Record Ownership.

---
+++ EdFi.Ods.Utilities.Migration/Scripts/MsSql/02Upgrade/v34_to_v50_RecordOwnership/1000-AddOwnershipColumnsForNewTables.sql
@@ -0,0 +1,61 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+-- Add RecordOwnership columns for newly added tables
+ALTER TABLE [edfi].[Person]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[Survey]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyCourseAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyProgramAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyQuestion]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyQuestionResponse]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyResponse]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyResponseEducationOrganizationTargetAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveyResponseStaffTargetAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveySection]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveySectionAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveySectionResponse]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveySectionResponseEducationOrganizationTargetAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
+
+ALTER TABLE [edfi].[SurveySectionResponseStaffTargetAssociation]
+    ADD [CreatedByOwnershipTokenId] SMALLINT NULL;
+GO
