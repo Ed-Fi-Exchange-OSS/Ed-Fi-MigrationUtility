559a44474ae2b704807392054f3013b5da2479b1
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2021-01-13 19:13:29 +0530

    More sql fixes for Postgres

--- EdFi.Ods.Utilities.Migration.PostgreSql.Tests/MigrationTests/Latest/GrandBendFullUpgradeTests/GrandBendCompatibilityUpdate.sql
+++ EdFi.Ods.Utilities.Migration.PostgreSql.Tests/MigrationTests/Latest/GrandBendFullUpgradeTests/GrandBendCompatibilityUpdate.sql
@@ -3,22 +3,22 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-UPDATE [edfi].[StaffCredential] SET StateOfIssueStateAbbreviationTypeId = 1 WHERE StateOfIssueStateAbbreviationTypeId IS NULL;
+UPDATE edfi.StaffCredential SET StateOfIssueStateAbbreviationTypeId = 1 WHERE StateOfIssueStateAbbreviationTypeId IS NULL;
 
-INSERT [edfi].[StudentEducationOrganizationAssociation]
+INSERT edfi.StudentEducationOrganizationAssociation
 (
-    [StudentUSI],
-    [EducationOrganizationId],
-    [ResponsibilityDescriptorId]
+    StudentUSI,
+    EducationOrganizationId,
+    ResponsibilityDescriptorId
 )
 VALUES
 (1, 255901, 618),
 (962, 255901, 618);
 
-UPDATE [edfi].[Assessment]
-SET [Namespace] = 'uri://ed-fi.org'
-WHERE [Namespace] IS NULL;
+UPDATE edfi.Assessment
+SET Namespace = 'uri://ed-fi.org'
+WHERE Namespace IS NULL;
 
-UPDATE [edfi].[LearningObjective]
-SET [Namespace] = 'http://ed-fi.org/LearningObjective/LearningObjective.xml';
+UPDATE edfi.LearningObjective
+SET Namespace = 'http://ed-fi.org/LearningObjective/LearningObjective.xml';
 
