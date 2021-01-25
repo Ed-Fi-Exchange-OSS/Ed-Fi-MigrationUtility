da9cff15ec4fc548971ba21fd63d1266ded5d481
semalai <m.semalaiappan@gmail.com>
2021-01-25 11:11:48 -0600

    for 5.0 to 5.1

---
+++ EdFi.Ods.Utilities.Migration.Tests/PgSql/MigrationTests/Latest/PostgresGrandDaleV50Tov51.cs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.Utilities.Migration.Enumerations;
+
+namespace EdFi.Ods.Utilities.Migration.Tests.PgSql.MigrationTests.Latest
+{
+    public class PostgresGrandDaleV50Tov51 : PostgreGrandDaleTests
+    {
+        protected override EdFiOdsVersion FromVersion => EdFiOdsVersion.V50;
+        protected override EdFiOdsVersion ToVersion => EdFiOdsVersion.V51;
+
+    }
+}
