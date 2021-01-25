1bfc15f2af782799c592c5648d547d94775e26f6
semalai <m.semalaiappan@gmail.com>
2021-01-25 13:41:19 -0600

    removing unwanted change

--- EdFi.Ods.Utilities.Migration.Tests/MsSql/MigrationTests/v2_to_v3/GlendaleTests/GlendaleTests.cs
+++ EdFi.Ods.Utilities.Migration.Tests/MsSql/MigrationTests/v2_to_v3/GlendaleTests/GlendaleTests.cs
@@ -8,31 +8,31 @@ using EdFi.Ods.Utilities.Migration.Enumerations;
 using EdFi.Ods.Utilities.Migration.Tests.Enumerations;
 using NUnit.Framework;
 
-namespace EdFi.Ods.Utilities.Migration.Tests.MsSql.MigrationTests.v2_to_v3.GlenDaleTests
+namespace EdFi.Ods.Utilities.Migration.Tests.MsSql.MigrationTests.v2_to_v3.GlendaleTests
 {
     [TestFixture]
-    public class GlenDaleTests : V2ToV3SqlServerMigrationTest
+    public class GlendaleTests : V2ToV3SqlServerMigrationTest
     {
         protected override DatabaseRestoreSetupOption DatabaseRestoreSetupOption { get; } = DatabaseRestoreSetupOption.RestoreDuringFixtureSetupOnly;
         protected override EdFiOdsVersion FromVersion => EdFiOdsVersion.V2UpTo24;
 
-        protected override string OptionalTestSourceOdsBackupFullPath => GetFullGlenDaleBackupPath();
+        protected override string OptionalTestSourceOdsBackupFullPath => GetFullGlendaleBackupPath();
 
-        protected override string TestDisabledReason => string.IsNullOrEmpty(GetFullGlenDaleBackupPath())
-            ? "GlenDale tests are currently optional due to disk space/runtime requirements. A GlenDale backup file was not supplied, so this test will be automatically disabled.  To enable, specify a valid backup file path in the application config"
+        protected override string TestDisabledReason => string.IsNullOrEmpty(GetFullGlendaleBackupPath())
+            ? "Glendale tests are currently optional due to disk space/runtime requirements. A Glendale backup file was not supplied, so this test will be automatically disabled.  To enable, specify a valid backup file path in the application config"
             : string.Empty;
 
-        private static string GetFullGlenDaleBackupPath()
+        private static string GetFullGlendaleBackupPath()
         {
-            return string.IsNullOrEmpty(SqlServerMigrationTestSettingsProvider.GetConfigVariable("GlenDaleBackupPath"))
+            return string.IsNullOrEmpty(SqlServerMigrationTestSettingsProvider.GetConfigVariable("GlendaleBackupPath"))
                 ? null
-                : Path.GetFullPath(SqlServerMigrationTestSettingsProvider.GetConfigVariable("GlenDaleBackupPath"));
+                : Path.GetFullPath(SqlServerMigrationTestSettingsProvider.GetConfigVariable("GlendaleBackupPath"));
         }
 
         [Test]
-        public void ShouldPassGlenDaleDefaultValidationWithNoErrors()
+        public void ShouldPassGlendaleDefaultValidationWithNoErrors()
         {
-            var upgradeResult = PerformTestMigration("GlenDaleCompatibilityUpdate.sql");
+            var upgradeResult = PerformTestMigration("GlendaleCompatibilityUpdate.sql");
             AssertUpgradeSucceeded(upgradeResult);
         }
     }
