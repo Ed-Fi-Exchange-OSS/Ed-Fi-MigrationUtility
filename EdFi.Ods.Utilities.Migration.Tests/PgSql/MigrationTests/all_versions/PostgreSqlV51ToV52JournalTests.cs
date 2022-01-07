278f5751b746f2a47dd7b92c2dd6cda35314f032
semalai <m.semalaiappan@gmail.com>
2022-01-07 16:24:57 -0600

    Updated Test method for Postgres 5.2

---
+++ EdFi.Ods.Utilities.Migration.Tests/PgSql/MigrationTests/all_versions/PostgreSqlV51ToV52JournalTests.cs
@@ -0,0 +1,20 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.Utilities.Migration.Enumerations;
+using EdFi.Ods.Utilities.Migration.Tests.Enumerations;
+using EdFi.Ods.Utilities.Migration.Tests.PgSql.MigrationTests.v51_to_v52;
+using NUnit.Framework;
+
+namespace EdFi.Ods.Utilities.Migration.Tests.PgSql.MigrationTests.all_versions
+{
+    [TestFixture]
+    public class PostgreSqlV51ToV52JournalTests : V51ToV52PostgreSqlMigrationTest
+    {
+        protected override DatabaseRestoreSetupOption DatabaseRestoreSetupOption { get; } = DatabaseRestoreSetupOption.FullRestoreBeforeEveryTest;
+        protected override EdFiOdsVersion FromVersion => EdFiOdsVersion.V51;
+        protected override EdFiOdsVersion ToVersion => EdFiOdsVersion.V52;
+    }
+}
