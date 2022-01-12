0be9f16588b8ee31d9837247f565507e071d2bc1
Ben Tillman <ben@headspring.com>
2022-01-12 15:03:35 -0600

    Re-org scripts since no need to have a separate script file for coursetranscript

--- EdFi.Ods.Utilities.Migration/Scripts/MsSql/02Upgrade/v52_to_v53/01 Structure/1020-RecreateCourseTranscriptTable.sql
+++
@@ -1,8 +0,0 @@
--- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
-ALTER TABLE [edfi].[CourseTranscript] ADD
-[ExternalEducationOrganizationNameOfInstitution] [nvarchar] (75) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-GO
--- EdFi.Ods.Utilities.Migration/Scripts/MsSql/02Upgrade/v52_to_v53/01 Structure/1030-ModifyExistingTables.sql
+++ EdFi.Ods.Utilities.Migration/Scripts/MsSql/02Upgrade/v52_to_v53/01 Structure/1030-ModifyExistingTables.sql
@@ -3,6 +3,10 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
+ALTER TABLE [edfi].[CourseTranscript] ADD
+[ExternalEducationOrganizationNameOfInstitution] [nvarchar] (75) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+GO
+
 DROP INDEX [FK_Credential_CredentialFieldDescriptor] ON [edfi].[Credential]
 GO
 
